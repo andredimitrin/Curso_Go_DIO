79b7d48d4f382055bcf23e560203bc9e02234515
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2023-11-09 20:15:58 -0300

    subindo mais att

--- src/estrutura_controle/loopH.go
+++ src/estrutura_controle/loopH.go
@@ -1,15 +1,32 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+
+// 	for horas := 0; horas <= 24; horas++ {
+// 		fmt.Println("Horas: ", horas)
+
+// 		for minutos := 0; minutos <= 59; minutos++ {
+// 			fmt.Println("Minutos: ", minutos)
+// 		}
+
+// 	}
+// }
+
+//substituindo o while com for
+
 package main
 
 import "fmt"
 
 func main() {
 
-	for horas := 0; horas <= 24; horas++ {
-		fmt.Println("Horas: ", horas)
+	i := 0
 
-		for minutos := 0; minutos <= 59; minutos++ {
-			fmt.Println("Minutos: ", minutos)
-		}
-
+	for i <= 20 {
+		fmt.Println("i é menor ou igual a 20")
+		i++
 	}
+
 }
