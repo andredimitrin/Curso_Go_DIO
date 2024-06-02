d2bc3d4a5a83b86e146fd33497aba4e1875acacc
André Dimitrin <82331544+andredimitrin@users.noreply.github.com>
2024-06-02 18:31:36 -0300

    att

---
+++ src/Resolvendo Problemas Numéricos com Go/desafio_numerico.go
@@ -0,0 +1,19 @@
+// Você e seus colegas querem criar uma código que exiba
+//todos os números entre 1 e 100 que são divisíveis por 3.
+
+package main
+
+import "fmt"
+
+func main() {
+
+	for i := 1; i <= 100; i++ {
+
+		if i%3 == 0 {
+
+			fmt.Println(i)
+
+		}
+	}
+
+}
